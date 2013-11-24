6acfe07d2d258a0db90c6c04285d7a8e6e159aed
kevlened <boyettel@gmail.com>
2013-11-24 18:04:40 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ sikuli_cpython
 ==============
 
 A CPython wrapper around the sikuli standalone jar
+
+Why
+---
+Sikuli is traditionally written in Jython, which is painful if you want to mix it with Selenium scripts. This module makes it possible for CPython to run the Jython scripts created by Sikuli-IDE.
+
+How it works
+------------
+This module wraps the sikuli standalone jar using [pyjnius](https://github.com/kivy/pyjnius) from the Kivy project. Pyjnius allows us to use java classes in CPython.
---
+++ sikuli/sikuli.py
@@ -0,0 +1,101 @@
+import os
+
+javapath = ":" + os.path.dirname(os.path.realpath(__file__)) + "/sikuli-api-1.0.2-standalone.jar"
+os.environ['CLASSPATH'] += javapath
+print 'CLASSPATH: ', os.environ['CLASSPATH']
+
+from jnius import autoclass
+
+File = autoclass('java.io.File')
+URL = autoclass('java.net.URL')
+
+DesktopMouse = autoclass('org.sikuli.api.robot.desktop.DesktopMouse')
+DesktopKeyboard = autoclass('org.sikuli.api.robot.desktop.DesktopKeyboard')
+
+API = autoclass('org.sikuli.api.API')
+DesktopScreenRegion = autoclass('org.sikuli.api.DesktopScreenRegion')
+ImageTarget = autoclass('org.sikuli.api.ImageTarget')
+
+
+def find(target):
+    t = ImageTarget(File(target))
+    d = DesktopScreenRegion()
+    x = d.find(t)
+    return x
+
+
+def findAll(target):
+    t = ImageTarget(File(target))
+    d = DesktopScreenRegion()
+    x = d.findAll(t)
+    return x
+
+
+def wait(target, duration=5000):
+    t = ImageTarget(File(target))
+    d = DesktopScreenRegion()
+    x = d.wait(t, duration)
+    return x
+
+
+# TODO: waitVanish
+
+
+def exists(target):
+    t = ImageTarget(File(target))
+    d = DesktopScreenRegion()
+    try:
+        x = d.find(t)
+    except Exception:
+        x = None
+
+    return x
+    
+
+def click(target):
+    t = ImageTarget(File(target))
+    d = DesktopScreenRegion()
+    item = d.find(t)
+    m = DesktopMouse()
+    m.click(item.getCenter())
+
+
+def doubleClick(target):
+    t = ImageTarget(File(target))
+    d = DesktopScreenRegion()
+    item = d.find(t)
+    m = DesktopMouse()
+    m.doubleClick(item.getCenter())
+
+
+def rightClick(target):
+    t = ImageTarget(File(target))
+    d = DesktopScreenRegion()
+    item = d.find(t)
+    m = DesktopMouse()
+    m.rightClick(item.getCenter())
+
+
+def hover(target):
+    t = ImageTarget(File(target))
+    d = DesktopScreenRegion()
+    item = d.find(t)
+    m = DesktopMouse()
+    m.hover(item.getCenter())
+
+
+# TODO: dragDrop
+
+
+def type(text):
+    k = DesktopKeyboard()
+    k.type(text)
+
+
+def paste(text):
+    k = DesktopKeyboard()
+    k.paste(text)
+
+
+def browse(url):
+    API.browse(URL(url))
