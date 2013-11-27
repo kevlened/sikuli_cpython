8088c56f40b177e6910fdf7cff36203242e7d3d1
Len Boyette <boyettel@gmail.com>
2013-11-26 21:32:30 -0500

    Added URL identifiers and dragDrop

--- sikuli/sikuli.py
+++ sikuli/sikuli.py
@@ -6,11 +6,11 @@ if sys.platform == 'win32':
 else:
     separator = ':'
     
-javapath = separator + os.path.dirname(os.path.realpath(__file__)) + "/sikuli-api-1.0.2-standalone.jar"
+jarpath = separator + os.path.dirname(os.path.realpath(__file__)) + "/sikuli-api-1.0.2-standalone.jar"
 if not 'CLASSPATH' in os.environ:
-    os.environ['CLASSPATH'] = javapath
+    os.environ['CLASSPATH'] = jarpath
 else:
-    os.environ['CLASSPATH'] += javapath
+    os.environ['CLASSPATH'] += jarpath
 
 from jnius import autoclass
 
@@ -25,74 +25,70 @@ DesktopScreenRegion = autoclass('org.sikuli.api.DesktopScreenRegion')
 ImageTarget = autoclass('org.sikuli.api.ImageTarget')
 
 
-def find(target):
-    t = ImageTarget(File(target))
-    d = DesktopScreenRegion()
-    x = d.find(t)
-    return x
+def find(target_string, *args, **kwargs):
+    target = _get_target_from_string(target_string, *args, **kwargs)
+    screen = DesktopScreenRegion()
+    return screen.find(target)
 
 
-def findAll(target):
-    t = ImageTarget(File(target))
-    d = DesktopScreenRegion()
-    x = d.findAll(t)
-    return x
+def findAll(target_string, *args, **kwargs):
+    target = _get_target_from_string(target_string, *args, **kwargs)
+    screen = DesktopScreenRegion()
+    return screen.findAll(target)
 
 
-def wait(target, duration=5000):
-    t = ImageTarget(File(target))
-    d = DesktopScreenRegion()
-    x = d.wait(t, duration)
-    return x
+def wait(target_string, *args, **kwargs):
+    if not duration and duration != 0:
+        duration = 5000
+    target = _get_target_from_string(target_string, *args, **kwargs)
+    screen = DesktopScreenRegion()
+    return screen.wait(target, duration)
 
 
 # TODO: waitVanish
 
 
-def exists(target):
-    t = ImageTarget(File(target))
-    d = DesktopScreenRegion()
+def exists(target_string, *args, **kwargs):
+    target = _get_target_from_string(target_string, *args, **kwargs)
+    screen = DesktopScreenRegion()
     try:
-        x = d.find(t)
+        t = screen.find(target)
     except Exception:
-        x = None
+        t = None
 
-    return x
+    return t
     
 
-def click(target):
-    t = ImageTarget(File(target))
-    d = DesktopScreenRegion()
-    item = d.find(t)
+def click(target_string, *args, **kwargs):
+    t = _get_screen_loc(target_string, *args, **kwargs)
     m = DesktopMouse()
-    m.click(item.getCenter())
+    m.click(t)
 
 
-def doubleClick(target):
-    t = ImageTarget(File(target))
-    d = DesktopScreenRegion()
-    item = d.find(t)
+def doubleClick(target_string, *args, **kwargs):
+    t = _get_screen_loc(target_string, *args, **kwargs)
     m = DesktopMouse()
-    m.doubleClick(item.getCenter())
+    m.doubleClick(t)
 
 
-def rightClick(target):
-    t = ImageTarget(File(target))
-    d = DesktopScreenRegion()
-    item = d.find(t)
+def rightClick(target_string, *args, **kwargs):
+    t = _get_screen_loc(target_string, *args, **kwargs)
     m = DesktopMouse()
-    m.rightClick(item.getCenter())
+    m.rightClick(t)
 
 
-def hover(target):
-    t = ImageTarget(File(target))
-    d = DesktopScreenRegion()
-    item = d.find(t)
+def hover(target_string, *args, **kwargs):
+    t = _get_screen_loc(target_string, *args, **kwargs)
     m = DesktopMouse()
-    m.hover(item.getCenter())
+    m.hover(t)
 
 
-# TODO: dragDrop
+def dragDrop(target_string_1, target_string_2):
+    loc1 = _get_screen_loc(target_string_1)
+    loc2 = _get_screen_loc(target_string_2)
+    m = DesktopMouse()
+    m.drag(loc1)
+    m.drop(loc2)
 
 
 def type(text):
@@ -107,3 +103,34 @@ def paste(text):
 
 def browse(url):
     API.browse(URL(url))
+
+
+def _get_screen_loc(target_string, *args, **kwargs):
+    t = find(target_string, *args, **kwargs)
+    return t.getCenter()  
+
+
+def _get_target_from_string(target_string, *args, **kwargs):
+    target_file_loc = _find_local_file(target_string)
+    if not target_file_loc:
+        try:        
+            target_file_loc = URL(target_string)
+        except:
+            target_file_loc = None
+                        
+    return ImageTarget(target_file_loc)
+
+
+def _find_local_file(target_string):
+    
+    # Check the relative path
+    poss_relative_path = os.path.join(os.getcwd(), target_string)
+    if os.path.exists(poss_relative_path):
+        return File(poss_relative_path)
+        
+    # Check the absolute path
+    elif os.path.exists(target_string):
+        return File(target_string)    
+    
+    else:
+        return None
